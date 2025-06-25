74fb6392cdc09bab329ada267a0a7f8382a542a6
William-L2006 <williamlopezparis@gmail.com>
2025-06-25 14:44:35 +0200

    Add files via upload

---
+++ questions_num.js
@@ -0,0 +1,128 @@
+var questions = [
+    {
+        "question_text": "Vrai ou faux : une console de jeux est un ordinateur",
+        "input_type": "radio",
+        "options": [
+            "Vrai",
+            "Faux"
+        ],
+        "answers": [
+            "Vrai"
+        ],
+        "detail": "Une console de jeux est un ordinateur spécialisé conçu uniquement pour jouer à des jeux vidéo. Elle possède des composants similaires à ceux des ordinateurs."
+    },
+    {
+        "question_text": "Comment appelle-t-on les petites images que l'on voit sur l'écran pour ouvrir un programme ?",
+        "input_type": "radio",
+        "options": [
+            "Icônes",
+            "Widgets",
+            "Onglets"
+        ],
+        "answers": [
+            "Icônes"
+        ],
+        "detail": "Les petites images que l'on voit sur l'écran pour lancer des programmes sont appelées des icônes."
+    },
+    {
+        "question_text": "Quel est l'outil utilisé pour dessiner directement sur un écran ?",
+        "input_type": "radio",
+        "options": [
+            "Stylet",
+            "Souris",
+            "Clavier"
+        ],
+        "answers": [
+            "Stylet"
+        ],
+        "detail": "Un stylet est un outil utilisé pour dessiner directement sur un écran"
+    },
+    {
+        "question_text": "Vrai ou faux : Une fois qu'on a créé un mot de passe sur un site",
+        "input_type": "radio",
+        "options": [
+            "Vrai",
+            "Faux"
+        ],
+        "answers": [
+            "Faux"
+        ],
+        "detail": "Les mots de passe sont spécifiques à chaque site ou service et doivent être créés pour chaque compte."
+    },
+    {
+        "question_text": "Comment s'appellent les ordinateurs qui stockent (ou \"hébergent\") les sites Internet ?",
+        "input_type": "radio",
+        "options": [
+            "Serveurs",
+            "Modems",
+            "Routeurs"
+        ],
+        "answers": [
+            "Serveurs"
+        ],
+        "detail": "Les serveurs sont des ordinateurs qui stockent et hébergent les sites Internet pour que les utilisateurs puissent y accéder à distance."
+    },
+    {
+        "question_text": "Vrai ou faux : Une IA comme ChatGPT est programmée pour dire toujours la vérité.",
+        "input_type": "radio",
+        "options": [
+            "Vrai",
+            "Faux"
+        ],
+        "answers": [
+            "Faux"
+        ],
+        "detail": "Les IA comme ChatGPT ne sont pas programmées pour dire la vérité. Elles génèrent des réponses en fonction de données"
+    },
+    {
+        "question_text": "Que signifie \"HD\" quand on parle de la qualité d'une vidéo ?",
+        "input_type": "radio",
+        "options": [
+            "Haute Définition",
+            "Haute Dynamique",
+            "Haute Durabilité"
+        ],
+        "answers": [
+            "Haute Définition"
+        ],
+        "detail": "HD signifie \"Haute Définition\""
+    },
+    {
+        "question_text": "Comment fonctionne le vibreur d'un téléphone ou d'une manette ?",
+        "input_type": "radio",
+        "options": [
+            "Avec un petit moteur qui tourne",
+            "Avec des ondes sonores",
+            "Avec des signaux électriques"
+        ],
+        "answers": [
+            "Avec un petit moteur qui tourne"
+        ],
+        "detail": "Le vibreur d'un téléphone ou d'une manette fonctionne grâce à un petit moteur qui tourne"
+    },
+    {
+        "question_text": "Vrai ou faux : tous les chargeurs de téléphones et tablettes fonctionnent avec le même voltage.",
+        "input_type": "radio",
+        "options": [
+            "Vrai",
+            "Faux"
+        ],
+        "answers": [
+            "Faux"
+        ],
+        "detail": "Tous les chargeurs n'ont pas le même voltage. Il est important d'utiliser le chargeur approprié pour chaque appareil."
+    },
+    {
+        "question_text": "Quel est le jeu vidéo avec le plus grand nombre de joueurs aujourd'hui ?",
+        "input_type": "radio",
+        "options": [
+            "Fortnite",
+            "Minecraft",
+            "League of Legends"
+        ],
+        "answers": [
+            "Minecraft"
+        ],
+        "detail": "Avec plus de 238 millions d'exemplaires vendus"
+    }
+];
